57378fa59b56528643786c150c48aa90dad42797
ManashSwain <manash0802@gmail.com>
2024-08-07 01:04:26 +0530

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+      '/' : {
+        target : "https://chatapp-backend-alpha.vercel.app" ,
+        changeOrigin : true ,
+        secure : false,
+      }
+    }
+  },
   plugins: [react()],
 })
